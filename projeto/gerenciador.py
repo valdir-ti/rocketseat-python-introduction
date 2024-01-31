f050614b7c76e5afd2960a9d779cdf07753d319b
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-31 18:58:57 -0300

    feat: update tarefa

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -7,11 +7,19 @@ def add_tarefa(tarefas, nome_tarefa):
 def ver_tarefas(tarefas):
     print("\nLista de tarefas: ")
     for index, tarefa in enumerate(tarefas, start=1):
-        status = "✓" if tarefa["completada"] else "✖"
+        status = "✓" if tarefa["completada"] else ""
         nome_tarefa = tarefa["tarefa"]
         print(f"{index}. [{status}] {nome_tarefa}")
         return
-    
+
+def atualizar_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+    indice_tarefa_ajustado = indice_tarefa - 1
+    if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa} atualizada para: {novo_nome_tarefa}")
+    else: 
+        print("Indice de tarefa inválido")
+    return    
 
 tarefas = []
 while True:
@@ -30,6 +38,11 @@ while True:
         add_tarefa(tarefas, nome_tarefa)    
     elif escolha == 2:
         ver_tarefas(tarefas)
+    elif escolha == 3:
+        ver_tarefas(tarefas)
+        indice_tarefa = int(input("Digite o número da tarefa que deseja atualizar: "))
+        novo_nome = input("Digite o novo nome da tarefa: ")
+        atualizar_tarefa(tarefas, indice_tarefa, novo_nome)
     elif escolha == 6:
         break
     else:
