21671ee7ea3a6d83a67585ff41a9b84700f51c39
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-31 18:32:19 -0300

    feat: add tarefa function

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,3 +1,11 @@
+tarefas = []
+
+def add_tarefa(tarefas, nome_tarefa):
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} adicionada com sucesso")
+    return
+
 while True:
     print("Menu do gerenciador de lista de tarefas")
     print("1. Adicionar de tarefa")
@@ -9,7 +17,13 @@ while True:
 
     escolha = int(input("Digite a sua escolha: "))
 
-    if escolha == 6:
+    if escolha == 1:
+        nome_tarefa = input("Digite o nome da nova tarefa: ")
+        add_tarefa(tarefas, nome_tarefa)
+    elif escolha == 6:
         break
+
+
+print(tarefas)
 
 print("Programa finalizado")
