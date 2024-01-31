c76590c47dca430f231419929bf9a0c1a362f309
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-31 18:15:11 -0300

    feat: add while with break

---
+++ projeto/gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+    print("Menu do gerenciador de lista de tarefas")
+    print("1. Adicionar de tarefa")
+    print("2. Ver tarefas")
+    print("3. Atualizar tarefa")
+    print("4. Completar tarefa")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
+
+    escolha = int(input("Digite a sua escolha: "))
+
+    if escolha == 6:
+        break
+
+print("Programa finalizado")
